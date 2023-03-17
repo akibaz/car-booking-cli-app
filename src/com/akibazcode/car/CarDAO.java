b24e1bee98cadcf5dcf0222c5dbd6a30f6a0e3ad
akibaz <bavakumovic@gmail.com>
2023-03-17 02:56:46 +0100

    clean up CarDAO

--- src/com/akibazcode/car/CarDAO.java
+++ src/com/akibazcode/car/CarDAO.java
@@ -16,36 +16,4 @@ class CarDAO {
     public static Car[] getCars() {
         return cars;
     }
-
-    public static Car[] getAvailableCars() {
-        Car[] availableCars = new Car[nextAvailableIndex];
-        int index = 0;
-        for (Car car : cars) {
-            if (car != null && car.isAvailable()) {
-                availableCars[index++] = car;
-            }
-        }
-        return availableCars;
-    }
-
-    public static Car[] getAvailableElectricCars() {
-        int count = 0;
-        for (Car car : cars) {
-            if (car != null && car.isElectric() && car.isAvailable()) {
-                count++;
-            } else {
-                break;
-            }
-        }
-        Car[] availableElectricCars = new Car[count];
-        int counter = 0;
-        for (Car car : cars) {
-            if (car != null && car.isElectric() && car.isAvailable()) {
-                availableElectricCars[counter++] = car;
-            } else {
-                break;
-            }
-        }
-        return availableElectricCars;
-    }
 }
